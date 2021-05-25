ed846107726a4dda350a28a119938aef20cc8bb5
sbmagar <sbmagar.sbm99@gmail.com>
2021-05-26 05:34:24 +0545

    added allowed_hosts

--- personal/wsgi.py
+++ personal/wsgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal.settings')
-
-application = get_wsgi_application()
+# import os
+#
+# from django.core.wsgi import get_wsgi_application
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal.settings')
+#
+# application = get_wsgi_application()
