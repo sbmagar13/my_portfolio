59560cdc6cb2005fb22773dcb167138a4cbfa2f6
sbmagar <sbmagar.sbm99@gmail.com>
2021-06-04 09:50:58 +0545

    Production and development docker configurations and heroku configurations

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 
-set -e # exit if errors happen anywhere
-python manage.py migrate
-python manage.py collectstatic --no-input --clear
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
 
-uwsgi --socket :8000 --master --enable-threads --module personal.wsgi
+    while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+# It's okay to run following two flush and migrate command on development mode.. when debug mode is 1 not recommended
+# for production:
+
+# python manage.py flush --no-input
+# python manage.py migrate
+
+
+#uwsgi --socket :8000 --master --enable-threads --module personal.wsgi
+
+exec "$@"
+
