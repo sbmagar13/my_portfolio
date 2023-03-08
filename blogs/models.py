0872db33ba62e9d372350ec9376881ee6a34bee4
Sagar Budhathoki Magar <sbmagar.sbm99@gmail.com>
2023-03-08 16:06:00 +0545

    resume link updated

--- blogs/models.py
+++ blogs/models.py
@@ -25,6 +25,9 @@ class Category_post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     verbose_name_plural = 'categories'
 
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
