d22b8f27585490adb14742a365942342c38cd3bb
Sagar Budhathoki Magar <sbmagar.sbm99@gmail.com>
2023-03-09 17:52:10 +0545

    model file updated

--- blogs/models.py
+++ blogs/models.py
@@ -81,7 +81,7 @@ class Comment(models.Model):
         ordering = ('created',)
 
     def __str__(self):
-        return f"{strip_tags(self.body[0:30])}... by {self.name} at {self.created.strftime('%d-%m-%Y')}"
+        return f"{strip_tags(self.body[0:30])}... by {self.name} at {self.created.strftime('%d %b, %Y')}"
 
 
 # class contact_us(models.Model):
